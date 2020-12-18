a7e920c3490d02431022da67db653073d277c0d5
agwa <agwa.dan@gmail.com>
2020-12-18 15:06:22 +0300

    00:43:00 Added fetch, delete and update routes for backend.

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -16,10 +16,10 @@ router.route('/').get((req, res) => {
 
 
 router.route('/add').post((req, res) => {
-    const username = req.body.username;
-    const description = req.body.description;
-    const duration = Number(req.body.duration);
-    const date = Date.parse(req.body.date);
+    const username      = req.body.username;
+    const description   = req.body.description;
+    const duration      = Number(req.body.duration);
+    const date          = Date.parse(req.body.date);
 
     //*********After getting all the variables above, a new exercise is created as below*********** */
     const newExercise = new Exercise({
@@ -30,8 +30,42 @@ router.route('/add').post((req, res) => {
     });
 
     newExercise.save()
-        .then(() => res.json('Exercise added'))
-        .catch(err => res.status(400).json('Error:' +err));
+        .then(()    => res.json('Exercise added'))
+        .catch(err  => res.status(400).json('Error:' +err));
 });
+
+
+//>>>>>>>>>>>>>>>>>>Router to fetch info about a given exercise by taking an id>>>>>>>>>>>> */
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise  => res.json(exercise))
+        .catch(err      => res.status(400).json('Error: ', +err));
+});
+
+
+//>>>>>>>>>>>>>>>>>>Router to delete a given exercise by taking an id>>>>>>>>>>>> */
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(()    => res.json('Exercise Deleted.'))
+        .catch(err  => res.status(400).json('Error: ', +err));
+});
+
+
+//>>>>>>>>>>>>>>>>>>Router to update info about a given exercise by taking an id>>>>>>>>>>>> */
+
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise =>{
+            exercise.username       = req.params.username;
+            exercise.description    = req.params.description;
+            exercise.duration       = Number(req.params.duration);
+            exercise.date           = Date.parse(req.params.date);
+
+            exercise.save()
+                .then(()    => res.json('Exercise updated!'))
+                .catch(err  => res.status(400).json('Error', +err));
+        })
+        .catch(err => res.status(400).json('Error: ', +err));
+})
 
 module.exports = router;
