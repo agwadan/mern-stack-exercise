c7632075d0805614712d6c41e7698b73463a1110
Agwa <agwa.dan@gmail.com>
2021-01-23 18:00:44 +0300

    Updated exercises and users models.

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -1,9 +1,8 @@
 /*********************BLUEPRINT FOR THE EXERCISES*************************/
 
 const mongoose  = require('mongoose');
-const Schema    = mongoose.Schema;
 
-const exerciseSchema = new Schema({//--------------------------- Contains 4 fields as seen below.
+const exerciseSchema = new mongoose.Schema({//--------------------------- Contains 4 fields as seen below.
     username    : {type: String, required: true},
     description : {type: String, required: true},
     duration    : {type: Number, required: true},
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,9 +3,7 @@
 
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;//----------------------------- new Schema is created as an instance of mongoose.schema 
-
-const userSchema = new Schema ({
+const userSchema = new mongoose.Schema ({
     username: {
         type        : String, 
         required    : true,
