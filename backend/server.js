31787cb10cdabda1e2a6f2a8369342e5c379e51d
Agwa <agwa.dan@gmail.com>
2021-02-01 22:38:16 +0300

    Clean up

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
-const express   = require('express');
-const cors      = require('cors'); //------------------------- cors allows access to resources outside the server
-const mongoose  = require('mongoose');//---------------------- Assists in connection to the MongoDB.
+const express = require('express');
+const cors = require('cors'); //------------------------- cors allows access to resources outside the server
+const mongoose = require('mongoose');//---------------------- Assists in connection to the MongoDB.
 
-require ('dotenv').config();
+require('dotenv').config();
 
 
 //**********CREATING THE EXPRESS SERVER******************/
 //______________________________________________________/
 
-const app   = express();
-const port  = process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 
 /******************MIDDLEWARE ***************************/
@@ -22,19 +22,20 @@ app.use(express.json());//------------------------------- Allows parsing of JSON
 
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, 
-                        useCreateIndex: true, 
-                        useUnifiedTopology: true});
-                        
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
 console.log("Mongoose... Connection to database successful.");
 
 
-const exerciseRouter    = require('./routes/exercises');
-const usersRouter       = require('./routes/users');
+const exerciseRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
 app.use('/exercises', exerciseRouter);//-------------------------------- Loads everything in the exercises router when the url entered includes /exercises.
 app.use('/users', usersRouter);//--------------------------------------- Loads everything in the users router when the url entered includes /users.
-
 
 /****STARTING THE SERVER**********/
 //___________________________________________________________/
