3f5e44fe320589f2bba79076f735b406caf6b116
Agwa <agwa.dan@gmail.com>
2021-02-01 22:52:04 +0300

    Add Login form

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
 export default function Login() {
+
+  const handleSubmit = () => {
+
+  }
   return (
     <div>
+      <form className='form' onSubmit={handleSubmit}>
 
+        <div className='form-group'>
+          <label>Name</label>
+          <input type='text' />
+        </div>
+
+        <div className='form-group'>
+          <label>Password</label>
+          <input type='text' />
+        </div>
+
+        <button className='btn btn-primary' type='submit'>Sign Up</button>
+
+      </form>
     </div>
   )
 }
