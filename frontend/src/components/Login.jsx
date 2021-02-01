8cef5cf3458e0d4517129ac70153b282585d650f
Agwa <agwa.dan@gmail.com>
2021-02-01 23:06:46 +0300

    Capture login info from frontend

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Login() {
 
-  const handleSubmit = () => {
+  const [user, setUser] = useState({
+    name: '',
+    password: ''
+  });
+  //const [password, setPassword] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
   }
   return (
     <div>
@@ -11,12 +18,20 @@ export default function Login() {
 
         <div className='form-group'>
           <label>Name</label>
-          <input className='form-control' type='text' />
+          <input
+            className='form-control'
+            type='text'
+            onChange={(e) => setUser({ ...user, name: e.target.value })}
+            value={user.name} />
         </div>
 
         <div className='form-group'>
           <label>Password</label>
-          <input className='form-control' type='text' />
+          <input
+            className='form-control'
+            type='password'
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })} />
         </div>
 
         <button className='btn btn-primary' type='submit'>Log In</button>
