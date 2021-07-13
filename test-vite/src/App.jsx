4b9084d6a794b7f62e3923c2e6fce9862707e349
agwadan <agwa.dan@gmail.com>
2021-07-13 17:02:53 +0300

    Change to vite provider for frontend

---
+++ test-vite/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/Navbar';
+import CreateExercise from './components/CreateExercise';
+import CreateUser from './components/CreateUser';
+import EditExercise from './components/EditExercise';
+import ExerciseList from './components/ExerciseList';
+import UserList from './components/UserList'
+import Login from './components/Login';
+
+function App() {
+  console.log('App running');
+  return (
+    <Router>
+      <div className='container'>
+        <NavBar />
+        <br />
+        <Route path='/' exact component={ExerciseList} />
+        <Route path='/exercises' exact component={ExerciseList} />
+        <Route path='/edit/:id' exact component={EditExercise} />
+        <Route path='/create' exact component={CreateExercise} />
+        <Route path='/user' exact component={CreateUser} />
+        <Route path='/users' exact component={UserList} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ test-vite/src/components/CreateUser.jsx
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class CreateUser extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onChangePasswordTwo = this.onChangePasswordTwo.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: '',
+      password: '',
+      passwordTwo: ''
+    }
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+  onChangePasswordTwo(e) {
+    this.setState({
+      passwordTwo: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault(); //--------------------- Prevents the forms from being handled the default way html does it.
+
+    const user = {
+      username: this.state.username,
+      password: this.state.passwordTwo
+    }
+    console.log(user);
+    axios.post('http://localhost:5000/users/add', user)
+      .then(res => console.log(res.data));
+
+    this.setState({ //------------------------- Resetting the username to nothing after the submission has been done.
+      username: '',
+      password: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className=" form-group">
+            <label>Username </label>
+            <input
+              type='text'
+              required
+              className='form-control'
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
+          </div>
+
+          <div className='form-group'>
+            <label>Password </label>
+            <input
+              type='password'
+              required
+              className='form-control'
+              value={this.state.password}
+              onChange={this.onChangePassword}
+            />
+          </div>
+
+          <div className='form-group'>
+            <label>Confirm Password </label>
+            <input
+              type='password'
+              required
+              className='form-control'
+              value={this.state.passwordTwo}
+              onChange={this.onChangePasswordTwo}
+            />
+          </div>
+
+          <div className='form-group'>
+            <label>Upload Profile Picture</label>
+            <input type='file' name='photo' />
+          </div>
+
+          <div className='form-group'>
+            <input type='submit' value='Create User' className='btn btn-primary' />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CreateUser;
